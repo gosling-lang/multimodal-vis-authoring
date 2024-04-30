d5090b190412741d0565e72eb86a752bfc58c2ff
sehilyi <sehilyi@gmail.com>
2024-04-29 21:15:38 -0400

    add base url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
   build: { target: 'esnext', outDir: 'build' },
+  base: '/multimodal-vis-authoring/',
   plugins: [reactRefresh()],
 });
