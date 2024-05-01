173ca6363909ffe3579dbab4999ad01fe12fa2fc
sehilyi <sehilyi@gmail.com>
2024-05-01 03:56:27 -0400

    add UI

--- src/App.tsx
+++ src/App.tsx
@@ -18,24 +18,31 @@ const Data = _data as Datum[];
 
 function App() {
 	return (
-		<div className="h-full">
-			<nav className="h-10 font-bold bg-black/10">
-				<h1>Multimodal Visualization Authoring</h1>
+		<div className="h-full bg-[#F6F6F6]">
+			<nav className="text-white h-[80px] bg-[#0072B2] shadow-md">
+				<h1 className="pl-3 pt-[9px] font-bold text-lg inline-block align-middle">Multimodal Interaction</h1>
+        <br/>
+				<h2 className="pl-3 pt-[4px] font-light text-sm inline-block align-middle">In Visualization Authoring Tools</h2>
 			</nav>
-			<div className="flex flex-rows outline outline-1 outline-black/20">
-				<div className="flex-none w-[200px] outline outline-1 outline-black/20"></div>
-				<div className="flex-1 outline outline-1 outline-black/20">
-					{Data.map((datum) => (
-						<div key={datum.id}>
-							<h2>{datum.title}</h2>
-						</div>
-					))}
-				</div>
-			</div>
-      <footer className="h-10 font-bold bg-black/10">
+			<div className="p-3 flex flex-wrap gap-4 h-[calc(100%-110px)] overflow-y-auto overflow-x-none">
+        {Data.map((datum) => (
+          <div className="m-0 border border-[grey] bg-[#F0F0F0] shadow cursor-pointer hover:shadow-lg" key={datum.id}
+            onClick={() => {
+              window.open(datum.url, '_blank');
+            }}
+          >
+            <h2 className="p-2 pl-3 font-medium text-xl w-[300px]">{datum.name}</h2>
+            <h3 className="p-2 pl-3 font text w-[300px]">{datum.title}</h3>
+            <h3 className="p-2 pl-3 font text-xs w-[300px]">{datum.authors.join(', ')}</h3>
+            <h3 className="p-2 pl-3 font text-xs w-[300px]">{datum.venue}, {datum.index}</h3>
+            <h3 className="p-2 pl-3 font-bold text-xs w-[300px]">{datum.year}</h3>
+          </div>
+        ))}
+      </div>
+      <footer className="h-[30px] font-bold bg-black sticky bottom-0">
       </footer>
-		</div>
-	);
+      </div>
+      );
 }
 
 export default App;
